8d6ae439ee5f361db61a8469d3049f20d6ef7377
Eason0628 <zhaowintoo@gmail.com>
2026-04-14 20:45:33 +0800

    2.Mapping Type

--- 1.BasicTsGrammer/7.Mapping.ts
+++ 1.BasicTsGrammer/7.Mapping.ts
@@ -3,6 +3,16 @@
 //基本使⽤
 type My="a"|"b"|"c"
 type MyType={[key in My]:number} //{a:number,b:number,c:number}
-let obj: MyType = {a: 1, b: 2, c: 3} 
 
-// ====================================================
+
+//配合keyof使⽤
+type Props={a:number,b:string,c:boolean}
+type MyType={[key in keyof Props]:Props[key]} //{a:number,b:string,c:boolean}
+let obj:MyType={a:123,b:"2",c:true}
+
+
+//配合可选属性使⽤
+type My="a"|"b"|"c"
+type MyType={[key in My]?:number} //{a?:number,b?:number,c?:number}
+let obj:MyType={a:123}
+
